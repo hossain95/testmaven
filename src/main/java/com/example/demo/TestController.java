abed19a9f516001990c382467641b2d600a5f6cb
hossain95 <miahossain8888@gmail.com>
2022-11-02 10:22:07 +0600

    test purpose

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/info")
+    public Info getInfo(){
+        Info info = new Info();
+        info.setName("Mia Hossain");
+        info.setEmail("miahossain@gmail.com");
+        return info;
+    }
+}
