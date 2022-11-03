f00cde34d82d9dcf73f1f794fda3c9b070d13b0c
hossain95 <miahossain8888@gmail.com>
2022-11-03 12:35:14 +0600

    add some code

--- src/main/java/com/example/demo/TestApplication.java
+++ src/main/java/com/example/demo/TestApplication.java
@@ -8,7 +8,7 @@ public class TestApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		SpringApplication.run(TestApplication.class, args);
+		SpringApplication.run(TestApplication.class, args)
 	}
 
 }
