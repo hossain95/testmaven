9e0b0d32051ea5d07b38dfbc520d4c5d3568a30a
Mia Hossain <95671729+miaExabyting@users.noreply.github.com>
2022-11-07 14:21:42 +0600

    Update TestApplication.java

--- src/main/java/com/example/demo/TestApplication.java
+++ src/main/java/com/example/demo/TestApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestApplication {
+public class TestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		SpringApplication.run(TestApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TestApplication.class);
+	}
 }
